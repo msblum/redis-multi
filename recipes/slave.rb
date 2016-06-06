c708bfee468d347c5d21ec8b928aae09428a789e
Matt Blum <msblum@users.noreply.github.com>
2016-06-06 17:13:11 -0400

    Update slave.rb

--- recipes/slave.rb
+++ recipes/slave.rb
@@ -18,9 +18,6 @@ master_ip = node['redis-multi']['redis_master']
 bind_port = node['redis-multi']['bind_port']
 
 # if downstream doesn't supply, do a nice default
-Chef::Log.warn('Checking if Redisio servers nil')
-if node.deep_fetch('redisio', 'servers').nil?
-  Chef::Log.warn('Redisio servers nil')
   master_data = { 'name' => "#{bind_port}-slave",
                   'port' => bind_port,
                   'slaveof' => { 'address' => master_ip,
@@ -30,9 +27,6 @@ if node.deep_fetch('redisio', 'servers').nil?
   Chef::Log.warn("Master data: #{master_data}")
   node.set['redisio']['servers'] = []
   node.set['redisio']['servers'] << master_data
-else
-  Chef::Log.warn("Redisio servers are #{node.deep_fetch('redisio', 'servers')}")
-end
 
 tag('redis_slave')
 tag('redis')
